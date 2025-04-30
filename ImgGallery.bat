978304907d1db41617ea42bed3a894af05f56c74
Dave <davewpolis@gmail.com>
2025-04-30 16:52:29 -0400

    Formatting, Code Comments, Cleanup
    - removed unused CSS entries
    - reworked CSS to improve grid view (alignment, padding, borders, flex-flow)
    - consolidated redundant for loops
    - added support for .bmp and .webp formats
    - added code comments
    - improved formatting

--- ImgGallery.bat
+++ ImgGallery.bat
@@ -1,101 +1,85 @@
 @ECHO OFF
-ECHO ImgGallery.bat v1.01 for Windows
-ECHO Displays all images in current folder in a HTML gallery.
+ECHO ImgGallery.bat v1.2 for Windows
+ECHO Displays all images in a folder as a HTML gallery
 ECHO.
-ECHO Based on Pict_NT.bat v2.00 by Rob van der Woude
+ECHO Originally based on Pict_NT.bat v2.00 by Rob van der Woude
 ECHO http://www.robvanderwoude.com
 ECHO.
 ECHO Enhanced by Dave
 ECHO https://github.com/DaveStuff/ImgGallery.git
 ECHO.
 
+REM set working directory to current directory (allows script to be run via console from another location)
 cd /D %CD%
 
 SETLOCAL
+
+REM define codepage/charset
 FOR /F "tokens=*" %%A IN ('CHCP') DO FOR %%B IN (%%A) DO SET CodePage=%%B
 IF %CodePage% LSS 1000 (SET CharSet=ibm%CodePage%) ELSE (SET Charset=windows-%CodePage%)
-SET OutFile="%~n0.htm"
->  %OutFile% ECHO ^<!DOCTYPE html^>
->> %OutFile% ECHO ^<html lang="en"^>
->> %OutFile% ECHO ^<html^>
->> %OutFile% ECHO ^<head^>
->> %OutFile% ECHO ^<title^>%CD%^</title^>
->> %OutFile% ECHO ^<meta http-equiv="content-type" content="text/html; charset=%CharSet%" /^>
->> %OutFile% ECHO 	^<style^>
->> %OutFile% ECHO 		body {
->> %OutFile% ECHO 			width: 100%;
->> %OutFile% ECHO 			margin: auto;
->> %OutFile% ECHO 			background-color: #000000
->> %OutFile% ECHO 		}
->> %OutFile% ECHO 		.gallery {
->> %OutFile% ECHO 			width: 100%;
->> %OutFile% ECHO 			display: flex;
->> %OutFile% ECHO 			flex-flow: row wrap;
->> %OutFile% ECHO 		}
->> %OutFile% ECHO 		.box {
->> %OutFile% ECHO 			flex-basis: 20%;
->> %OutFile% ECHO 			width: 100%;
->> %OutFile% ECHO 			padding: 10px;
->> %OutFile% ECHO 			margin: 8px;
->> %OutFile% ECHO 		}
->> %OutFile% ECHO 		.text {
->> %OutFile% ECHO 			text-align: center;
->> %OutFile% ECHO 			margin-top: 5px;
->> %OutFile% ECHO 		}
->> %OutFile% ECHO 		@media only screen and (max-width: 300px) {
->> %OutFile% ECHO 			.box {
->> %OutFile% ECHO 				flex-basis: 100%;
->> %OutFile% ECHO 			}
->> %OutFile% ECHO 		}
->> %OutFile% ECHO 		@media only screen and (max-width:500px) {
->> %OutFile% ECHO 	 		.box {
->> %OutFile% ECHO 				flex-basis: 40%;
->> %OutFile% ECHO 			}
->> %OutFile% ECHO 		}
->> %OutFile% ECHO 	^</style^>
->> %OutFile% ECHO ^</head^>
->> %OutFile% ECHO 	^<body^>
->> %OutFile% ECHO 		^<div class="gallery"^>
 
-FOR %%A IN (*.GIF) DO (
-	>> %OutFile% ECHO 			^<div class="box"^>
-	>> %OutFile% ECHO 				^<div class="image"^>
-	>> %OutFile% ECHO 					^<a target="_blank" href="%%~nxA"^>
-	>> %OutFile% ECHO 						^<img src="%%~nxA" width="300"/^>
-	>> %OutFile% ECHO 					^</a^>
-	>> %OutFile% ECHO 				^</div^>
-	>> %OutFile% ECHO 			^</div^>
-	)
-FOR %%A IN (*.JPG) DO (
-	>> %OutFile% ECHO 			^<div class="box"^>
-	>> %OutFile% ECHO 				^<div class="image"^>
-	>> %OutFile% ECHO 					^<a target="_blank" href="%%~nxA"^>
-	>> %OutFile% ECHO 						^<img src="%%~nxA" width="300"/^>
-	>> %OutFile% ECHO 					^</a^>
-	>> %OutFile% ECHO 				^</div^>
-	>> %OutFile% ECHO 			^</div^>
-	)
-FOR %%A IN (*.JPEG) DO (
-	>> %OutFile% ECHO 			^<div class="box"^>
-	>> %OutFile% ECHO 				^<div class="image"^>
-	>> %OutFile% ECHO 					^<a target="_blank" href="%%~nxA"^>
-	>> %OutFile% ECHO 						^<img src="%%~nxA" width="300"/^>
-	>> %OutFile% ECHO 					^</a^>
-	>> %OutFile% ECHO 				^</div^>
-	>> %OutFile% ECHO 			^</div^>
-	)
-FOR %%A IN (*.PNG) DO (
-	>> %OutFile% ECHO 			^<div class="box"^>
-	>> %OutFile% ECHO 				^<div class="image"^>
-	>> %OutFile% ECHO 					^<a target="_blank" href="%%~nxA"^>
-	>> %OutFile% ECHO 						^<img src="%%~nxA" width="300"/^>
-	>> %OutFile% ECHO 					^</a^>
-	>> %OutFile% ECHO 				^</div^>
-	>> %OutFile% ECHO 			^</div^>
-	)
->> %OutFile% ECHO 		^</div^>
->> %OutFile% ECHO ^</body^>
->> %OutFile% ECHO ^</html^>
-START "ImgGallery.bat" %OutFile%
+REM set name of output HTML file ("%~n0.htm" uses the same base name as this batch file)
+SET OutFile="%~n0.htm"
+
+REM HTML
+	>  %OutFile% ECHO ^<!DOCTYPE html^>
+	>> %OutFile% ECHO ^<html lang="en"^>
+	>> %OutFile% ECHO ^<html^>
+	>> %OutFile% ECHO ^<head^>
+
+	REM use current directory as page title
+	>> %OutFile% ECHO ^<title^>%CD%^</title^>
+
+	REM CSS
+		>> %OutFile% ECHO 	^<style^>
+		>> %OutFile% ECHO 		body {
+		>> %OutFile% ECHO 			margin: auto;
+		>> %OutFile% ECHO 			background-color: #707070;
+		>> %OutFile% ECHO 		}
+ 		>> %OutFile% ECHO 		.gallery {
+		>> %OutFile% ECHO 			margin: 20px;
+		>> %OutFile% ECHO 			padding: 10px;
+ 		>> %OutFile% ECHO 			display: flex;
+ 		>> %OutFile% ECHO 			flex-flow: row wrap;
+ 		>> %OutFile% ECHO 		}
+		>> %OutFile% ECHO 		.box {
+		>> %OutFile% ECHO 			margin: 5px;
+		>> %OutFile% ECHO			display: flex;
+		>> %OutFile% ECHO 			justify-content: center;
+ 		>> %OutFile% ECHO 			align-items: center;
+		>> %OutFile% ECHO 		}
+ 		>> %OutFile% ECHO 		.image {
+		>> %OutFile% ECHO 			padding-top: 4px;
+		>> %OutFile% ECHO 			padding-left: 4px;
+		>> %OutFile% ECHO 			padding-right: 4px;
+		>> %OutFile% ECHO 			background-color: #171717;
+		>> %OutFile% ECHO 		}
+		>> %OutFile% ECHO 	^</style^>
+	REM /CSS 
+	
+	>> %OutFile% ECHO ^</head^>
+		
+	REM GALLERY
+		>> %OutFile% ECHO 	^<body^>
+		>> %OutFile% ECHO 		^<div class="gallery"^>	
+			REM add images in working folder to the gallery (GIF, JPG, JPEG, PNG, BMP, WEBP)
+			FOR %%A IN (*.GIF *.JPG *.JPEG *.PNG *.BMP *.WEBP) DO (
+				>> %OutFile% ECHO 			^<div class="box"^>
+ 				>> %OutFile% ECHO 				^<div class="image"^>
+				>> %OutFile% ECHO 					^<a target="_blank" href="%%~nxA"^>
+				>> %OutFile% ECHO 						^<img src="%%~nxA" width="300"/^>
+				>> %OutFile% ECHO 					^</a^>
+ 				>> %OutFile% ECHO 				^</div^>
+				>> %OutFile% ECHO 			^</div^>
+				)
+		>> %OutFile% ECHO 		^</div^>
+		>> %OutFile% ECHO ^</body^>
+	REM /GALLERY
+	>> %OutFile% ECHO ^</html^>
+REM /HTML
+
+REM open gallery in default browser
+START "" %OutFile%
+
 ENDLOCAL
 GOTO:EOF
